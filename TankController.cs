37945e10563ae6122551913fbb9e6e3d4a0f16f8
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-01-28 21:10:45 +0700

    added new script
    tankconttroler,weaponscript,shootablebox,and rayviewer

---
+++ TankController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour {
+	public Camera playerCam;
+	public Camera tankCam;
+	public KeyCode enterVehicle = KeyCode.F;
+	// Use this for initialization
+	void Start () {
+		playerCam.enabled = true;
+		tankCam.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (enterVehicle)) {
+			ShowTankCamera ();
+		} else if(Input.GetKey(KeyCode.Escape)){
+			ShowPlayerCamera ();
+		}
+	}
+
+	public void ShowTankCamera() {
+		playerCam.enabled = false;
+		tankCam.enabled = true;
+	}
+
+	public void ShowPlayerCamera() {
+		playerCam.enabled = true;
+		tankCam.enabled = false;
+	}
+}
